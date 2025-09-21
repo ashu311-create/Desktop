3ee3ebcf197f213828afbd1ee8a7a6e002a93f92
ashu311-create <aarushasthanal6278@gmail.com>
2025-09-21 16:33:34 +0300

    v1

---
+++ Python-Module 1/Lesson 2/project2.py
@@ -0,0 +1,3 @@
+x=November_3_2010
+y=July_17_2009
+z=February_
---
+++ Python-Module 1/Lesson 3/Activity 2.py
@@ -0,0 +1,10 @@
+age=15
+is_student=True
+weight=60.6
+name="Rahul"
+age=str(age)
+print(age)
+print("data type of age",type(age))
+weight=int(weight)
+print(weight)
+print("data type of weight",type(weight))
