0fd3e2a820f569c092a35238caec0a93aecd75a3
ashu311-create <aarushasthanal6278@gmail.com>
2025-10-01 22:30:48 +0300

    v1

---
+++ Python-Module 1/Lesson 5/Project 5.py
@@ -0,0 +1,7 @@
+whether_temprature=int(input("What's the temprature today?"))
+if whether_temprature>20:
+    print("Wear some light clothes today")
+    print("It appears to be sunny today")
+else:
+    print("Wear some protective clothes today")
+    print("It appears to be cold today")
