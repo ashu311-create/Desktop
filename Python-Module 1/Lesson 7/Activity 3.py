ec5ec32aba336977614f8a43d4ef9038941ed151
ashu311-create <aarushasthanal6278@gmail.com>
2025-10-12 16:21:45 +0300

    v1

---
+++ Python-Module 1/Lesson 7/Activity 3.py
@@ -0,0 +1,28 @@
+print ("Enter your marks of 5 subjects here: ")
+mark1=int(input())
+mark2=int(input())
+mark3=int(input())
+mark4=int(input())
+mark5=int(input())
+total=mark1+mark2+mark3+mark4+mark5
+avg=total/5
+if avg>=91 and avg<=100:
+    print("Your grade is A1")
+elif avg>=81 and avg<91:
+    print("Your grade is A2")
+elif avg>=71 and avg<81:
+    print ("Your grader is B1")
+elif avg>=61 and avg<71:
+    print("Your grade is B2")
+elif avg>=51 and avg<61:
+    print ("Your grade is C1")
+elif avg>=41 and avg<51:
+    print ("Your grade is C2")
+elif avg>=33 and avg<41:
+    print ("Your grade is D")
+elif avg>=21 and avg<33:
+    print ("Your grade is E1")
+elif avg>=0 and avg<21:
+    print ("Your grade is E2")
+else:
+    print ("Invalid Input")
