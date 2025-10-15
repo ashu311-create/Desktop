5e6e0fd0f32b3c617ab6d14a04cb587fab364410
ashu311-create <aarushasthanal6278@gmail.com>
2025-10-15 23:52:00 +0300

    v1

---
+++ Python-Module 1/Lesson 6/Project6.py
@@ -0,0 +1,5 @@
+charecter=input("Enter a charectr: ")
+if (charecter>='a' and charecter<='z') or (charecter>='A' or charecter<='Z'):
+    print("This is an Alphabet")
+else:
+    print("Invalid input")
---
+++ Python-Module 1/Lesson 8/Mean number.py
@@ -0,0 +1,10 @@
+mean1=38
+misread_num=36
+correct_num=56
+total=40
+sum=mean1*total
+print("The sum of 40 numbers is",sum)
+num2=sum+((misread_num)-(correct_num))
+print("sum-((misread_num)-(correct_num)):",num2)
+mean2=num2/total
+print(mean2)
