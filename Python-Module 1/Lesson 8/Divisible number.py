f80e437ccce82670431362b09c732ab2d94eabd1
ashu311-create <aarushasthanal6278@gmail.com>
2025-10-15 16:10:21 +0300

    v1

---
+++ Python-Module 1/Lesson 8/Divisible number.py
@@ -0,0 +1,8 @@
+print("Enter a number (numerator):")
+numn=int(input())
+print("Enter a number (Denominator):")
+numd=int(input())
+if numn%numd==0:
+    print ("The numerator is divisible by the denominator")
+else:
+    print ("The numerator is not divisible by the denominator")
