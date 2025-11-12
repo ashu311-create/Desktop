3f1505c5970fdee39cf112afd72229d6d9002d73
ashu311-create <aarushasthanal6278@gmail.com>
2025-11-12 16:07:55 +0300

    v1

---
+++ Python-Module 2/Lesson 14-Intro to Turtle/Activity2.py
@@ -0,0 +1,12 @@
+import turtle
+t=turtle.Screen()
+t.bgcolor("white")
+t.setup(400,300)
+t.title("Welcome to Turtle")
+board=turtle.Turtle()
+board.right(75)
+board.forward(100)
+for i in range (4):
+    board.right(144)
+    board.forward(100)
+turtle.done ()
