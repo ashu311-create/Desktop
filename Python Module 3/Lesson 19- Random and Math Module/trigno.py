c28b63a979c0ec747a5fa34b27e73287c6ae0019
ashu311-create <aarushasthanal6278@gmail.com>
2026-03-10 23:08:34 +0300

    v1

---
+++ Python Module 3/Lesson 19- Random and Math Module/trigno.py
@@ -0,0 +1,10 @@
+import math
+degrees = 30
+radians = math.radians(degrees)
+sine_val = math.sin(radians)
+cosine_val = math.cos(radians)
+tangent_val = math.tan(radians)
+print(f"Angle: {degrees}°")
+print(f"Sin: {sine_val:.4f}")
+print(f"Cos: {cosine_val:.4f}")
+print(f"Tan: {tangent_val:.4f}")
