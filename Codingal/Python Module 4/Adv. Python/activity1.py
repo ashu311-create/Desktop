7258957fb8003067992e242619c95e9e596019d8
ashu311-create <aarushasthanal6278@gmail.com>
2026-03-12 14:01:20 +0300

    v1

---
+++ Codingal/Python Module 4/Adv. Python/activity1.py
@@ -0,0 +1,12 @@
+#Hands on map#
+num1=[1,2,3]
+num2=[4,5,6]
+result=map(lambda x, y:x+y,num1, num2)
+print("Addition of two lists")
+print(list(result))
+nums=[1,2,3,4,5]
+def sq(n):
+    return n*n
+square=list (map(sq,nums))
+print ("Squares of the numbers of the list:")
+print (square)
