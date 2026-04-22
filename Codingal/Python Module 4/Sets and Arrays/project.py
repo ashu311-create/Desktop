6abd739963cc5a72aae92c2a882654bc5e8bad45
ashu311-create <aarushasthanal6278@gmail.com>
2026-04-22 16:28:00 +0300

    v1

---
+++ Codingal/Python Module 4/Sets and Arrays/project.py
@@ -0,0 +1,8 @@
+set1 = {'blue', 'green'}
+set2 = {'blue', 'yellow'}
+result_A = set1.symmetric_difference(set2)
+print("Symmetric Difference (A):", result_A)
+set3 = {1, 2, 3, 4, 5}
+set4 = {1, 5, 6, 7, 8, 9}
+result_B = set3.symmetric_difference(set4)
+print("Symmetric Difference (B):", result_B)
